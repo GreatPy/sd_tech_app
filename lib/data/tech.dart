4b0d3fe86377b7dfdf578a3e25f185c28344d85d
GreatPy <v.serdjanian@gmail.com>
2023-11-02 14:40:22 +0100

    initialisation of tech model and a tech example

---
+++ lib/data/tech.dart
@@ -0,0 +1,11 @@
+import 'package:sd_tech/models/tech.dart';
+
+Tech vahe = const Tech(
+  firstname: "Vahé",
+  lastname: "Serdjanian",
+  phone: "06 00 00 00 00",
+  mail: "vs@sleepdoctor.me",
+  address: "45 rue Pierre Charron, 75008 PARIS",
+  pg: 3,
+  psg: 1,
+);
---
+++ lib/models/tech.dart
@@ -0,0 +1,18 @@
+class Tech {
+  const Tech({
+    required this.firstname,
+    required this.lastname,
+    required this.phone,
+    required this.mail,
+    required this.address,
+    required this.pg,
+    required this.psg,
+  });
+  final String firstname;
+  final String lastname;
+  final String phone;
+  final String mail;
+  final String address;
+  final int pg;
+  final int psg;
+}
