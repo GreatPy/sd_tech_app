c49736c6f9ff8f30a44d8bd08f96405cfa329b02
GreatPy <v.serdjanian@gmail.com>
2023-11-14 10:53:19 +0100

    clean up

---
+++ lib/widgets/generals/typed_exam_box/typed_costumer_infos.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:sd_tech/models/enums/exam_costumer_info_line.dart';
+import 'package:sd_tech/models/typed_exam.dart';
+import 'package:sd_tech/widgets/generals/exam_box/scrolable_row.dart';
+
+class CostumerInfos extends StatelessWidget {
+  const CostumerInfos({
+    super.key,
+    required this.exam,
+  });
+  final TypedExam exam;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ScrolableRow(
+          line: ExamCostuerInfoLine.fullName,
+          exam: exam.roughExam,
+        ),
+        ScrolableRow(
+          line: ExamCostuerInfoLine.city,
+          exam: exam.roughExam,
+        ),
+        ScrolableRow(
+          //if status == tobeScheduled:line = delay ,else: line = time
+          line: ExamCostuerInfoLine.delay,
+          exam: exam.roughExam,
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/widgets/generals/typed_exam_box/typed_scrolable_row.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:sd_tech/models/enums/exam_costumer_info_line.dart';
+import 'package:sd_tech/models/typed_exam.dart';
+import 'package:sd_tech/widgets/generals/styled_text.dart';
+
+class ScrolableRow extends StatelessWidget {
+  const ScrolableRow({
+    super.key,
+    required this.line,
+    required this.exam,
+  });
+  final ExamCostuerInfoLine line;
+  final TypedExam exam;
+
+  @override
+  Widget build(BuildContext context) {
+    final Widget content;
+    final DateTime today = DateTime.now();
+    switch (line) {
+      case ExamCostuerInfoLine.fullName:
+        content = Row(children: [
+          StyledText(content: exam.roughExam.customer.firstname),
+          const SizedBox(width: 8),
+          StyledText(content: exam.roughExam.customer.lastname)
+        ]);
+      case ExamCostuerInfoLine.city:
+        content = StyledText(content: exam.roughExam.customer.city);
+      case ExamCostuerInfoLine.delay:
+        Duration delay = today.difference(exam.roughExam.createdAt);
+        content = Row(
+          children: [
+            StyledText(content: delay.inDays.toString()),
+            const SizedBox(width: 8),
+            StyledText(
+                content: delay > const Duration(days: 2) ? "jours" : "jour")
+          ],
+        );
+      default:
+        content = const Placeholder();
+    }
+    return Row(
+      children: [
+        Expanded(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: content,
+          ),
+        ),
+      ],
+    );
+  }
+}
