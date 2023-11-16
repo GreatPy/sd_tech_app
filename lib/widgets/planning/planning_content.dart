0dcc616fea751d14f442e80024055afebc3d361b
GreatPy <v.serdjanian@gmail.com>
2023-11-16 16:15:59 +0100

    clean up

--- lib/widgets/planning/planning_content.dart
+++ lib/widgets/planning/planning_content.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:sd_tech/models/typed_exams.dart';
-import 'package:sd_tech/widgets/generals/exam_box/shift.dart';
 
 class PlanningContent extends StatelessWidget {
   const PlanningContent({super.key});
